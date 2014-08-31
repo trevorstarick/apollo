751c13cde84b270e5d59f1b4350cb6ffe65068ef
Trevor Starick <trevor.starick@gmail.com>
2014-08-31 13:37:00 -0400

    API changes to root instead of /modules/

--- server/app.js
+++ server/app.js
@@ -8,7 +8,10 @@ var routes = require('./routes.js');
 
 var app = express();
 app.use( bodyParser.urlencoded({ extended: false }));
-
+app.use(function(req, res, next){
+  console.log('%s %s', req.method, req.url);
+  next();
+});
 
 express.response.jsonp = function(obj) {
   var replacer = app.get('json replacer');
@@ -33,10 +36,21 @@ express.response.jsonp = function(obj) {
   return this.send(body);
 };
 
+app.all('*', function(req, res, next){
+  if (!req.get('Origin')) return next();
+  // use "*" here to accept any origin
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', '*');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  if ('OPTIONS' == req.method) return res.send(200);
+  next();
+});
+
 app.get('/', function(req, res){
   res.send('hello world!');
 });
 
-app.use('/modules', routes);
+app.use('/', routes);
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
