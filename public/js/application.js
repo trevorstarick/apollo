409358e018d7d74e900c53348162e2b70e9aa6b3
Trevor Starick <trevor.starick@gmail.com>
2014-09-05 00:55:06 -0400

    Application.js Pop v1

--- public/js/application.js
+++ public/js/application.js
@@ -1,97 +1,186 @@
 var remoteServer = 'http://localhost:3000';
-var $ = window.jQuery;
 
-(function() {
-  function loadModule(name) {
-    var template = '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 module" id="{{name}}"></div>';
-    template = template.replace('{{name}}', name);
-    $('#modules .row').append(template);
-    console.log('Added', name);
-    $('#' + name).append('<h1>' + name + '</h1>');
-  }
+(function(window, $) {
+  var ø = window.ø || {};
 
-  function graph(name) {
-    function tick() {
-            data.push(random());
-            path
-              .attr("d", line)
-              .attr("transform", null)
-              .transition()
-              .duration(60) //in ms
-            .ease("linear")
-              .attr("transform", "translate(" + x(-1) + ",0)")
-              .each("end", tick);
+  ø.Helpers = {
+    encodeID: function(string) {
+      string = string.replace(':','-58-');
+      return string;
+    },
+    decodeID: function(string) {
+      string = string.replace('-58-', ':');
+      return string;
+    },
+    lastItem: function(object) {
+      return object.data[object.data.length-1];
+    }
+  };
 
-            data.shift();
-          }
+  ø.Module = {
+    load: function(object) {
+      
+      var div = {};
+      var template = '';
+      var a, b, c, d;
 
-          var n = 40,
-            random = d3.random.normal(0, 1),
-            data = d3.range(n).map(random);
+      var x = 12;
 
-          var svgModule = d3.select('#' + name);
-          var margin = {
-              top: 50,
-              right: -10,
-              bottom: 50,
-              left: -10
-            },
-            width = parseInt(svgModule.style("width")),
-            // height = parseInt(svgModule.style("height"));
-            height = 50;
+      a = x/ 1;
+      b = x/ 2;
+      c = x/ 3;
+      d = x/ 4;
 
-          var svg = svgModule.append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + height + ")");
+      div.id = ø.Helpers.encodeID(object.name);
+      div.cl = "col-xs-"+a+" col-sm-"+b+" col-md-"+c+" col-lg-"+d+" module";
+      
+      for(var i in Object.keys(div)) {
+        var k = Object.keys(div)[i];
+        var v = div[k];
+      }
 
-          var x = d3.scale.linear()
-            .domain([0, n - 1])
-            .range([0, width]);
+      template += '<div id="'+div.id+'"class="'+div.cl+'">';
+      template += '<h1>' + ø.Helpers.decodeID(div.id) + '</h1>';
+      template += '<div class="content"></div>';
+      template += '</div>';
 
-          var y = d3.scale.linear()
-            .domain([-1, 1])
-            .range([height, 0]);
+      $('#modules .row').append(template);
+      console.log('Added', div.id);
+    },
 
-          var line = d3.svg.line()
-            .interpolate("basis")
-            .x(function(d, i) {
-              return x(i);
-            })
-            .y(function(d, i) {
-              return y(d);
-            });
+    update: function(object) {
+      console.log('update',object);
+      $('#'+ø.Helpers.encodeID(object.name)+' .content').html('');
+      if(!ø.Type[object.type]) {
+        throw 'Missing object.type for '+object.name+'. Got '+object.type+'!';
+      } else {
+        ø.Type[object.type](object);
+      }
+    },
 
-          var path = svg.append("g")
-            .attr("clip-path", "url(#clip)")
-            .append("path")
-            .datum(data)
-            .attr("class", "line")
-            .attr("d", line);
-          tick();
-  }
+    fetch: function(cb) {
+      $.getJSON(remoteServer + "/list", function(data) {
+        return cb(data.keys);
+      });
+    }
+  };
 
-  function bool(name) {
-    $('#' + name).append('<div class="boolAnswer">YES</div>');
-  }
+  ø.Type = {
+    float: function(object) {
+      function tick() {
+        data.push(random());
+        path
+          .attr("d", line)
+          .attr("transform", null)
+          .transition()
+          .duration(250) //in ms
+        .ease("linear")
+          .attr("transform", "translate(" + x(-1) + ",0)")
+          .each("end", tick);
 
-  function fetchModuleList() {
-    $.getJSON(remoteServer + "/list", function(data) {
-      $.each(data.keys, function(k, v) {
-        loadModule(v.name);
-        switch(v.type) {
-          case 'float':
-            graph(v.name);
-            break;
-          case 'bool':
-            bool(v.name);
-            break;
-          default:
-            throw 'Missing v.type for '+v.name+'. Got '+v.type+'!';
+        data.shift();
+      }
+
+      var n = 40,
+        random = d3.random.normal(0, 1),
+        data = d3.range(n).map(random);
+
+      var svgModule = d3.select('#' + ø.Helpers.encodeID(object.name) + ' .content');
+      var margin = {
+          top: 0,
+          right: -15,
+          bottom: 0,
+          left: -15
+        },
+        width = parseInt(svgModule.style("width")),
+        // height = parseInt(svgModule.style("height"));
+        height = 50;
+
+      var svg = svgModule.append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height * 2)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + height/2 + ")");
+
+      var x = d3.scale.linear()
+        .domain([0, n - 1])
+        .range([0, width]);
+
+      var y = d3.scale.linear()
+        .domain([-1, 1])
+        .range([height, 0]);
+
+      var line = d3.svg.line()
+        .interpolate("basis")
+        .x(function(d, i) {
+          return x(i);
+        })
+        .y(function(d, i) {
+          return y(d);
+        });
+
+      var path = svg.append("g")
+        .attr("clip-path", "url(#clip)")
+        .append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
+      tick();
+    },
+
+    bool: function(object) {      
+      var res = '';
+      if(object.data.length === 0) {
+        res = '<div class="moduleText warnText">N/A</div>';
+      } else { 
+        if(
+          ø.Helpers.lastItem(object) === 1 ||
+          ø.Helpers.lastItem(object) === true ||
+          ø.Helpers.lastItem(object) === 'true' ||
+          ø.Helpers.lastItem(object) === 'True'
+        ) {
+          res = '<div class="moduleText greenText">YES</div>';
+        } else {
+          res = '<div class="moduleText redText">NO</div>';
         }
+      }
+      $('#' + ø.Helpers.encodeID(object.name) + ' .content').append(res);
+    },
+
+    int: function(object) {
+      var res = '<div class="moduleText">'+ø.Helpers.lastItem(object)+'</div>';
+      $('#' + ø.Helpers.encodeID(object.name) + ' .content').append(res);
+    }
+  };
+
+  var lastModuleCount = 0;
+
+  ø.Module.fetch(function(data) {
+    lastModuleCount = data.length;
+    $.each(data, function(k, v) {
+      ø.Module.load(v);
+      ø.Module.update(v);
+    });
+  });
+
+  var updateInterval = 1 * 1000;
+
+  var updateID = window.setInterval(function(){
+    ø.Module.fetch(function(data) {
+      if (lastModuleCount !== data.length) {
+        if(lastModuleCount > data.length) {
+          console.log('something was removed!');
+        } else {
+          console.log('something was added');
+        }
+
+        lastModuleCount = data.length;
+      }
+
+      $.each(data, function(k, v) {
+        ø.Module.update(v);
       });
     });
-  }
-  fetchModuleList();
-}).call();
+  }, updateInterval);
+
+})(window,$);
